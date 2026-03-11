789a327f25c9b6834c9b856496df0a1ca447b0ee
ast2481 <universaltrendspaint@gmail.com>
2026-03-11 11:49:32 -0400

    Add project files.

---
+++ ocean level rise2.cpp
@@ -0,0 +1,20 @@
+// ocean level rise2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    double risePerYear = 1.5;
+
+    int Levelyear5 = 5;
+    int levelyear7 = 7;
+    int levelyear10 = 10;
+        
+    double rise5 = 1.5 * Levelyear5;
+    double rise7 = 1.5 * levelyear7;
+    double rise10 = 1.5 * levelyear10;
+
+   cout <<   
+}
+
